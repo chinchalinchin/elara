284e89508f11b122dead8604f57e112a2aec9507
chinchalinchin <chinchalinchin@gmail.com>
2024-12-06 15:31:48 -0500

    turn off acls for distro log bucket

--- env/main.tf
+++ env/main.tf
@@ -10,23 +10,39 @@ provider "aws" {
     region                      = "us-east-1"
 }
 
-
-module "bucket" {
-    source                      = "github.com/cumberland-terraform/storage-s3.git"
-    
+locals {
     platform                    = {
         client                  = "CUMBERLAND CLOUD"
         environment             = "PRODUCTION"
     }
+    kms                         = {
+        aws_managed             = true
+    }
+}
+
+module "bucket" {
+    source                      = "github.com/cumberland-terraform/storage-s3.git"
     
+    platform                    = local.platform
+    kms                         = local.kms
     s3                          = {
         purpose                 = "Hosting of static web content for the Elara website"
         suffix                  = "elara"
-        kms_key                 = {
-            aws_managed         = true
-        }
         website_configuration   = {
             enabled             = true
         } 
     }
+}
+
+module "distribution" {
+    source                      = "github.com/cumberland-terraform/network-cdn.git"
+    
+    platform                    = local.platform
+    kms                         = local.kms
+    s3                          = module.bucket.bucket[0]
+    cdn                         = {
+        domain                  = "elara.chinchalinchin.com"
+        name                    = "elara"
+    }
+
 }
