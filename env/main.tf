30add0f56be3190e68908db238ae4dfeb4c43529
chinchalinchin <chinchalinchin@gmail.com>
2024-12-06 11:07:25 -0500

    set up infra and web shell

---
+++ env/main.tf
@@ -0,0 +1,31 @@
+terraform {
+    backend "s3" {
+        bucket                  = "cumberland-cloud-terraform-state"
+        key                     = "web/elara/terraform.tfstate"
+        region                  = "us-east-1"
+    }
+}
+
+provider "aws" {
+    region                      = "us-east-1"
+}
+
+module "bucket" {
+    source                      = "github.com/cumberland-terraform/storage-s3.git"
+    
+    platform                    = {
+        client                  = "CUMBERLAND CLOUD"
+        environment             = "PRODUCTION"
+    }
+    
+    s3                          = {
+        purpose                 = "Hosting of static web content for Elara's website"
+        suffix                  = "elara"
+        kms_key                 = {
+            aws_managed         = true
+        }
+        website_configuration   = {
+            enabled             = true
+        } 
+    }
+}
---
+++ src/source/index.rst
@@ -0,0 +1,20 @@
+.. elara documentation master file, created by
+   sphinx-quickstart on Fri Dec  6 10:50:55 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to elara's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
